d7d71ed8eaa732e173f1c3e04f2516dffc6a511e
Seismicman <franciscosantelys@gmail.com>
2023-02-17 10:44:44 -0400

    latest changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import barcode from "../assets/images/image-qr-code.png";
 
 const Card = () => {
   return (
     <div className="card">
       <div className="qr">
-        <img src="./src/assets/images/image-qr-code.png" alt="" />
+        <img src={barcode} alt="" />
         <div className="text">
           <h2>Improve your front-end skills by building projects</h2>
           <p>
